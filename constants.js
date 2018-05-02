ff301a952a9cc7b92638177ed29d49ed6b58b021
Tony Sun <sunstryder@outlook.com>
2018-05-02 11:01:50 +1000

    export consts

--- constants.js
+++ constants.js
@@ -1,29 +1,23 @@
-// Endpoints for the dnd5e resources
-
-const BASE_ENDPOINT = 'http://dnd5eapi.co/api/';
+// Endpoints
+export const BASE_ENDPOINT = 'http://dnd5eapi.co/api/';
 
 // Pass an Index ONLY
-const ABILITY_SCORES = BASE_ENDPOINT+'ability-scores/';
-const SKILLS = BASE_ENDPOINT+'skills/';
-const LANGUAGES = BASE_ENDPOINT+'languages/';
-const FEATURES = BASE_ENDPOINT+'features/';
-const RACES = BASE_ENDPOINT+'races/';
-const EQUIPMENT = BASE_ENDPOINT+'equipment/';
-const CONDITIONS = BASE_ENDPOINT+'conditions/';
-const DAMAGE_TYPES = BASE_ENDPOINT+'damage-types/';
-const MAGIC_SCHOOLS = BASE_ENDPOINT+'magic-schools/';
-
+export const ABILITY_SCORES = BASE_ENDPOINT+'ability-scores/';
+export const SKILLS = BASE_ENDPOINT+'skills/';
+export const LANGUAGES = BASE_ENDPOINT+'languages/';
+export const FEATURES = BASE_ENDPOINT+'features/';
+export const RACES = BASE_ENDPOINT+'races/';
+export const EQUIPMENT = BASE_ENDPOINT+'equipment/';
+export const CONDITIONS = BASE_ENDPOINT+'conditions/';
+export const DAMAGE_TYPES = BASE_ENDPOINT+'damage-types/';
+export const MAGIC_SCHOOLS = BASE_ENDPOINT+'magic-schools/';
 
 // Pass an Index or  querystring
+export const SPELLS = BASE_ENDPOINT+'spells/';
+export const PROFICIENCIES = BASE_ENDPOINT+'proficiencies/';
+export const CLASSES = BASE_ENDPOINT+'classes/';
+export const SUBCLASSES = BASE_ENDPOINT+'subclasses/';
+export const SUBRACES = BASE_ENDPOINT+'subraces/';
 
-const SPELLS = BASE_ENDPOINT+'spells/';
-const PROFICIENCIES = BASE_ENDPOINT+'proficiencies/';
-const CLASSES = BASE_ENDPOINT+'classes/';
-const SUBCLASSES = BASE_ENDPOINT+'subclasses/';
-const SUBRACES = BASE_ENDPOINT+'subraces/';
-const LEVEL = CLASSES+'{class name}/level/{integer 1-20}';
-
-
-
-console.log (SPELLS_ENDPOINT);
-
+//Level endpoint needs to be handled differently since it is bisected by 2 params.
+// export const LEVEL = CLASSES+'{class name}/level/{integer 1-20}';
