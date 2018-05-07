c9aa6cac6c69dd52ac0901303681325d3af81396
Tony Sun <sunstryder@outlook.com>
2018-05-07 14:51:51 +1000

    add skills to Minerva's repertoire

--- express.js
+++ express.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 
-const { SPELLS } = require('./constants');
+const { SPELLS, SKILLS } = require('./constants');
 const spells = require('./spells');
-// spells.getSpellIndex to grab the index
+const skills = require('./skills');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -30,6 +30,23 @@ app.post('/spells', async (req, res) => {
             responseType: 'json'
         });
         let output = spells.spellParser(response.data);
+        return res.send(output);
+    }
+});
+
+app.post('/skills', async (req, res) => {
+    // POST request for skills
+    let skillIndex = skills.getSkillIndex(req.body.text);
+    if (skillIndex === -1) {
+        return res.send("Skill Does not exist. Perhaps you've misspelt it, it's a Spell, or I haven't learnt it yet :( ")
+    }else {
+        const response = await axios({
+            method: 'get',
+            url: SKILLS + skillIndex,
+            responseEncoding: 'utf8',
+            responseType: 'json'
+        });
+        let output = skills.skillParser(response.data);
         return res.send(output);
     }
 });
---
+++ skills.js
@@ -0,0 +1,57 @@
+const _ = require('lodash');
+
+const skillMapping = {
+    "acrobatics":1,
+    "animal handling":2,
+    "arcana":3,
+    "athletics":4,
+    "deception":5,
+    "history":6,
+    "insight":7,
+    "intimidation":8,
+    "investigation":9,
+    "medicine":10,
+    "nature":11,
+    "perception":12,
+    "performance":13,
+    "persuasion":14,
+    "religion":15,
+    "sleight of hand":16,
+    "stealth":17,
+    "survival":18,
+};
+
+
+// Input: skill name from this array
+// Output: skill index from this array.
+exports.getSkillIndex = (skillName) => {
+    const lowerSpellName = skillName.toLowerCase();
+    const skillIndex = _.get(skillMapping, lowerSpellName);
+    return skillIndex ? skillIndex : -1;
+};
+
+//SpellParser formats the JSON response of a skill resource.
+
+// TODO add error handling for mistyped skill names
+exports.skillParser = (skill) => {
+    return {
+        attachments: [
+            {
+                "title": `${skill.name}`,
+                "text": `\n\n _${skill.desc}_\n`,
+                "fields": [
+                    {
+                        "title": "\n\nAbility Score",
+                        "value": `${skill.ability_score.name}`,
+                        "short": true
+                    },
+                ],
+                "mrkdwn_in": [
+                    "text",
+                    "title"
+                ],
+                "color": "#88499c"
+            }
+        ],
+    }
+};
